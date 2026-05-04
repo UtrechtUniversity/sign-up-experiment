4a6135681d88a90fb9b49467891a99473e386424
robfranken <76061100+robfranken@users.noreply.github.com>
2026-05-04 11:41:09 +0200

    Update __init__.py

--- intro/__init__.py
+++ intro/__init__.py
@@ -29,7 +29,7 @@ class Player(BasePlayer):
 
     time_slots = models.StringField(
         choices=[
-            "13:00–13:30 Europe/Amsterdam (12:00–12:30 UK time)"
+            "14:00–14:30 Europe/Amsterdam (13:00–13:30 UK time)"
             #"13:30–14:30",
             #"15:00–16:00",
         ],
